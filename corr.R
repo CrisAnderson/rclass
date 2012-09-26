20ba00d20ad456c365add1da900a0f81cedd51b1
Cris Anderson <anderson861@gmail.com>
2012-09-26 00:27:50 -0700

    now calculates complete cases for all files in dir. need to implement selective logging and correlation.

--- corr.R
+++ corr.R
@@ -1,4 +1,38 @@
 corr <- function(directory, threshold = 0) {
+    filelist <- list.files(directory)
+
+    counter <- 0
+    oz <- '0'
+    tz <- '00'
+    results <- c()
+
+    for (a in filelist) {
+        counter <- 0
+
+        #if (nchar(a)==1) filen <- paste(tz,a,sep="")
+        #if (nchar(a)==2) filen <- paste(oz,a,sep="")
+        #if (nchar(a)==3) filen <- a
+
+        #filelocation <- paste(directory,"/",filen,".csv",sep="")
+        filelocation <- paste(directory,"/",a,sep="")
+
+        df <- read.csv(filelocation)
+
+        leng <- nrow(df)
+
+        for (i in 1:leng) { 
+            if (is.na(df[i,'sulfate']) | is.na(df[i,'nitrate']))
+                counter <- counter + 1
+        }
+
+        result <- (leng - counter)
+        results <- append(results, result)
+    }
+
+output <- data.frame(id = filelist, nobs = results)
+
+return(output)
 
 }
+
 
