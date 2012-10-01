b8f3f5e52754972e77c6d80832b9241348e196a0
Cris Anderson <anderson861@gmail.com>
2012-10-01 09:08:03 -0700

    Fixed up corr.R and got it working

--- corr.R
+++ corr.R
@@ -1,42 +1,30 @@
 corr <- function(directory, threshold = 0) {
     filelist <- list.files(directory)
 
-    shortlist <- c()
-
-    counter <- 0
     oz <- '0'
     tz <- '00'
-    results <- c()
+    corvalues <- c()
 
     for (a in filelist) {
-        counter <- 0
 
-        #if (nchar(a)==1) filen <- paste(tz,a,sep="")
-        #if (nchar(a)==2) filen <- paste(oz,a,sep="")
-        #if (nchar(a)==3) filen <- a
-
-        #filelocation <- paste(directory,"/",filen,".csv",sep="")
         filelocation <- paste(directory,"/",a,sep="")
 
         df <- read.csv(filelocation)
 
-        leng <- nrow(df)
+        result <- nrow(na.omit(df)) - 1
 
-        for (i in 1:leng) { 
-            if (is.na(df[i,'sulfate']) | is.na(df[i,'nitrate']))
-                counter <- counter + 1
-        }
 
-        result <- (leng - counter)
         if (result >= threshold) {
-            results <- append(results, result)
-            shortlist <- append(shortlist, a)
+            temp <- na.omit(df)
+            corvalues <- append(corvalues,cor(temp$sulfate, temp$nitrate))
         }
     }
 
-output <- data.frame(id = shortlist, nobs = results)
+if (length(corvalues) == 0) {
+    corvalues <- numeric(length = 0)
+}
 
-return(output)
+return(corvalues)
 
 }
 
